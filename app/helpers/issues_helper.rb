8f5bb67aedb4b04f4d3f091ea57c0a423aad9b95
dengxuefeng <dengxuefeng@localhost.localdomain>
2019-05-31 10:48:23 +0800

    修改邮件显示内容

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -312,16 +312,16 @@ module IssuesHelper
   def email_issue_attributes(issue, user, html)
     items = []
     # %w(author status priority assigned_to category fixed_version).each do |attribute|
-    %w(assigned_to).each do |attribute|
-      unless issue.disabled_core_fields.include?(attribute+"_id")
-        if html
-          items << content_tag('strong',"设备负责人: ") + (issue.send attribute)
-        else
-          items << "#{l("field_#{attribute}")}: #{issue.send attribute}"
-        end
-      end
-    end
-    issue.visible_custom_field_values(user).each do |value|
+    # %w(assigned_to).each do |attribute|
+    #   unless issue.disabled_core_fields.include?(attribute+"_id")
+    #     if html
+    #       items << content_tag('strong',"设备负责人: ") + (issue.send attribute)
+    #     else
+    #       items << "#{l("field_#{attribute}")}: #{issue.send attribute}"
+    #     end
+    #   end
+    # end
+    issue.visible_custom_field_values(user).select{|v| [1,2,3,4,5,7].include?(v.custom_field.id)}.each do |value|
       if html
         items << content_tag('strong', "#{value.custom_field.name}: ") + show_value(value, false)
       else
